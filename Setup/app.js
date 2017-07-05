39c1b2a18f2c68ddb2a1321b0e27364dd5c2278a
Tchontk <tchontk@gmail.com>
2017-07-05 23:31:00 +0200

    .

--- Setup/app.js
+++ Setup/app.js
@@ -2,68 +2,78 @@ new Vue({
   el: '#app',
   data: {
     combat: false,
-    hero: [{
+    players: [{
+      name: "HERO",
       health: 100,
       healthInit: 100,
-      attackDmg: [1, 5],
-      specialAttackDmg: [10, 20],
-      healValue: [50, 50]
+      attack: [1, 5],
+      specialAttack: [10, 20],
+      heal: [5, 10],
+      playerTarget: 1
     }, {
+      name: "MONSTER",
       health: 100,
       healthInit: 100,
-      attackDmg: [1, 5],
-      specialAttackDmg: [10, 20],
+      attack: [1, 5],
+      specialAttack: [10, 20],
+      heal: [5, 10],
+      playerTarget: 0
     }],
-    log: []
+    logs: []
   },
   watch: {
     combat: function () {
       console.log("hey");
-      if (this.hero[0].health <= 0 || this.hero[1].health <= 0) {
+      if (this.players[0].health <= 0 || this.players[1].health <= 0) {
         alert('stop')
       }
     }
   },
   computed: {
     heroHealth: function () {
-      console.log('hero : ', this.hero[0].health);
+      console.log('Hero : ', this.players[0].health);
       return {
-        width: Math.min((this.hero[0].health / this.hero[0].healthInit) * 100, 100) + '%'
+        width: (this.players[0].health / this.players[0].healthInit) * 100 + '%'
       }
     },
     monsterHealth: function () {
-      console.log('Monster : ', this.hero[1].health);
+      console.log('Monster : ', this.players[1].health);
       return {
-        width: Math.min((this.hero[1].health / this.hero[1].healthInit) * 100, 100) + '%'
+        width: (this.players[1].health / this.players[1].healthInit) * 100 + '%'
       }
     }
   },
   methods: {
     rndRangeValue: function (array) {
-      return ((array[1] - array[0]) * Math.random()) + array[0];
+      return Math.round((((array[1] - array[0]) * Math.random()) + array[0]), 2);
     },
-    attack: function () {
-      this.hero[0].health -= this.rndRangeValue(this.hero[1].attackDmg)
-      this.hero[1].health -= this.rndRangeValue(this.hero[0].attackDmg)
+    logAttack: function (dmg, attacker, defender, action) {
+      this.logs.push(attacker + ' ' + action + ' ' + defender + ' FOR ' + dmg)
     },
-    specialAttack: function () {
-      this.hero[0].health -= this.rndRangeValue(this.hero[1].specialAttackDmg)
-      this.hero[1].health -= this.rndRangeValue(this.hero[0].specialAttackDmg)
+    attack: function (actionName) {
+      this.players.forEach(function (player, index) {
+        let dmg = this.rndRangeValue(player[actionName])
+        let idTarget = player.playerTarget
+        this.players[idTarget].health = Math.max(this.players[idTarget].health - dmg, 0)
+        this.logAttack(dmg, player.name, this.players[idTarget].name, actionName)
+      }, this);
     },
-    heal: function () {
-      this.hero[0].health -= this.rndRangeValue(this.hero[1].specialAttackDmg)
-      this.hero[0].health += Math.min(this.rndRangeValue(this.hero[0].healValue), this.hero[0].healthInit)
+    heal: function (actionName) {
+      this.players.forEach(function (player, index) {
+        let dmg = this.rndRangeValue(player[actionName])
+        let heal = player.health
+        player.health = Math.min(heal + dmg, player.healthInit)
+        dmg = player.health - heal;
+        this.logAttack(dmg, player.name, player.name, actionName)
+      }, this);
     },
     giveUp: function () {
       this.combat = false
     },
     startGame: function () {
       this.combat = true
-      this.hero[0].health = this.hero[0].healthInit;
-      this.hero[1].health = this.hero[1].healthInit;
-      console.log(this.combat);
-      console.log(this.hero);
+      this.players[0].health = this.players[0].healthInit;
+      this.players[1].health = this.players[1].healthInit;
     }
   }
-
 })
