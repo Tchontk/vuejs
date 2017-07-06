70be0c72b97c77661240d43748a8f37906144e93
Tchontk <tchontk@gmail.com>
2017-07-06 13:02:41 +0200

    final

--- Setup/app.js
+++ Setup/app.js
@@ -23,9 +23,8 @@ new Vue({
   },
   watch: {
     combat: function () {
-      console.log("hey");
       if (this.players[0].health <= 0 || this.players[1].health <= 0) {
-        alert('stop')
+        (confirm('Restart ?') ? this.startGame() : null)
       }
     }
   },
@@ -34,12 +33,14 @@ new Vue({
       console.log('Hero : ', this.players[0].health);
       return {
         width: (this.players[0].health / this.players[0].healthInit) * 100 + '%'
+        /* text */
       }
     },
     monsterHealth: function () {
       console.log('Monster : ', this.players[1].health);
       return {
         width: (this.players[1].health / this.players[1].healthInit) * 100 + '%'
+        /* text */
       }
     }
   },
@@ -47,15 +48,13 @@ new Vue({
     rndRangeValue: function (array) {
       return Math.round((((array[1] - array[0]) * Math.random()) + array[0]), 2);
     },
-    logAttack: function (dmg, attacker, defender, action) {
-      this.logs.push(attacker + ' ' + action + ' ' + defender + ' FOR ' + dmg)
-    },
     attack: function (actionName) {
       this.players.forEach(function (player, index) {
         let dmg = this.rndRangeValue(player[actionName])
         let idTarget = player.playerTarget
         this.players[idTarget].health = Math.max(this.players[idTarget].health - dmg, 0)
-        this.logAttack(dmg, player.name, this.players[idTarget].name, actionName)
+        this.logs.push(player.name + ' HITS ' + this.players[idTarget].name + ' FOR ' + dmg)
+        this.combat = (this.players[idTarget].health <= 0 ? false : true)
       }, this);
     },
     heal: function (actionName) {
@@ -64,16 +63,18 @@ new Vue({
         let heal = player.health
         player.health = Math.min(heal + dmg, player.healthInit)
         dmg = player.health - heal;
-        this.logAttack(dmg, player.name, player.name, actionName)
+        this.logs.push(player.name + ' HEALS HIMSELF  FOR ' + dmg)
       }, this);
     },
     giveUp: function () {
       this.combat = false
     },
     startGame: function () {
+      this.logs = []
+      this.players.forEach(function (player) {
+        player.health = player.healthInit;
+      }, this);
       this.combat = true
-      this.players[0].health = this.players[0].healthInit;
-      this.players[1].health = this.players[1].healthInit;
     }
   }
 })
