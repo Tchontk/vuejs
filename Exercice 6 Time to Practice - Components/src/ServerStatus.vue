aedff6a9e329feacf216e796bd2f7e7a71c05292
Tchontk <tchontk@gmail.com>
2017-07-23 00:41:54 +0200

    Ex 6 ma solution

---
+++ Exercice 6 Time to Practice - Components/src/ServerStatus.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <app-server-status-detail></app-server-status-detail>
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ServerStatusDetail from './ServerStatusDetail.vue'
+export default {
+  components: {
+    'app-server-status-detail': ServerStatusDetail
+  }
+
+}
+</script>
