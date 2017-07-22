d1ca1d3202fed197cea56ff8a160e24f454f564c
Tchontk <tchontk@gmail.com>
2017-07-23 00:51:53 +0200

    Ex 6 correction

--- Exercice 6 Time to Practice - Components/src/App.vue
+++ Exercice 6 Time to Practice - Components/src/App.vue
@@ -2,8 +2,28 @@
   <div class="container">
     <app-header></app-header>
     <hr>
-    <app-server-status></app-server-status>
+    <div class="row">
+      <app-server-status-detail></app-server-status-detail>
+      <app-servers-status></app-servers-status>
+    </div>
     <hr>
     <app-footer></app-footer>
   </div>
 </template>
+
+<script>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import ServersStatus from './ServersStatus.vue'
+import ServerStatusDetail from './ServerStatusDetail.vue'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-servers-status': ServersStatus,
+    'app-server-status-detail': ServerStatusDetail
+  }
+
+}
+</script>
--- Exercice 6 Time to Practice - Components/src/main.js
+++ Exercice 6 Time to Practice - Components/src/main.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Header from './Header.vue'
-import Footer from './Footer.vue'
-import ServerStatus from './ServerStatus.vue'
-
-Vue.component('app-header', Header)
-Vue.component('app-server-status', ServerStatus)
-Vue.component('app-footer', Footer)
 
 new Vue({
   el: '#app',
