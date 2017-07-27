c99b391e74bf6ec0ebb956f1b10f2d5c3a4d9b3e
Tchontk <tchontk@gmail.com>
2017-07-27 12:43:10 +0200

    Props

--- 08 - Communicating between Components/src/components/UserDetail.vue
+++ 08 - Communicating between Components/src/components/UserDetail.vue
@@ -2,13 +2,28 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>User Name : {{myName}}</p>
+    <p>User Name : {{ myName }}</p>
+    <p>User Name Reverse : {{ switchName() }}</p>
+  
   </div>
 </template>
 
 <script>
 export default {
-  props: ['myName']
+  //props: ['myName'],
+  props: {
+    //myName: [String, Array]
+    myName: {
+      type: String,
+      required: true
+      //default: "Hello"
+    }
+  },
+  methods: {
+    switchName() {
+      return this.myName.split("").reverse().join("")
+    }
+  }
 }
 </script>
 
