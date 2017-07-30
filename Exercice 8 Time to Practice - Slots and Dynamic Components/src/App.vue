595675e64cf7c07e6657dcf217b3e9be6c3bd124
Tchontk <tchontk@gmail.com>
2017-07-30 15:35:26 +0200

    ex 08 end

--- Exercice 8 Time to Practice - Slots and Dynamic Components/src/App.vue
+++ Exercice 8 Time to Practice - Slots and Dynamic Components/src/App.vue
@@ -1,33 +1,42 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
-                <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br>
+        <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+        <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+        <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+        <hr>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <p>{{message}}</p>
+          </component>
+        </keep-alive>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+import Blue from './components/Blue.vue';
+import Green from './components/Green.vue';
+import Red from './components/Red.vue';
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
+export default {
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  },
+  data: function () {
+    return {
+      message: 'This is the content',
+      selectedComponent: 'appBlue'
     }
+  }
+}
 </script>
 
 <style>
+
 </style>
