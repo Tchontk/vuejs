d85dfccb85cb92b19df402bf62710e2eb904b4f5
Tchontk <tchontk@gmail.com>
2017-10-21 01:15:50 +0200

    Ex 10

--- Exercice 10 Time to Practice - Directives/src/App.vue
+++ Exercice 10 Time to Practice - Directives/src/App.vue
@@ -4,9 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
                 <p v-myOn:click="'Hey'">Color this</p>
+                <button v-customOn:click="clicked" class="btn btn-primary">Click Me</button>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <hr>
+                  <div style="width: 100px; heigth: 100px; background-color: lightgreen" v-customOn:mouseleave="mouseLeave" v-customOn:mouseenter="mouseEnter">!</div>
             </div>
         </div>
     </div>
@@ -17,14 +19,31 @@ export default {
   directives: {
     myOn: {
       bind(element, binding, vnode) {
-        console.log(binding.arg);
-        console.log(binding.modifiers);
-        if (binding.arg === "click") {
-          element.addEventListener("click", function() {
-            alert(binding.value);
-          });
-        }
+        element.addEventListener(binding.arg, function() {
+          alert(binding.value);
+        });
       }
+    },
+    customOn: {
+      bind(el, binding) {
+        // el.onclick = function() {
+        //   binding.value();
+        // };
+        const type = binding.arg;
+        const fn = binding.value;
+        el.addEventListener(type, fn);
+      }
+    }
+  },
+  methods: {
+    clicked() {
+      alert("I was clicked");
+    },
+    mouseEnter() {
+      console.log("Mouse Entered");
+    },
+    mouseLeave() {
+      console.log("Mouse left");
     }
   }
 };
