b7c0f8cd521ae6ad651545c321b32c4e6c253ab5
Tchontk <tchontk@gmail.com>
2017-10-21 00:26:43 +0200

    v1

--- Exercice 10 Time to Practice - Directives/src/App.vue
+++ Exercice 10 Time to Practice - Directives/src/App.vue
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
+                <p v-myOn:click="'Hey'">Color this</p>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
 
@@ -12,8 +13,23 @@
 </template>
 
 <script>
-    export default {}
+export default {
+  directives: {
+    myOn: {
+      bind(element, binding, vnode) {
+        console.log(binding.arg);
+        console.log(binding.modifiers);
+        if (binding.arg === "click") {
+          element.addEventListener("click", function() {
+            alert(binding.value);
+          });
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style>
+
 </style>
