4f3be3d0a66776b085007760fd764217ab8fcca3
Tchontk <tchontk@gmail.com>
2017-11-09 13:30:14 +0100

    Mixins

--- 13 - Improving your App with Filters and Mixins/src/App.vue
+++ 13 - Improving your App with Filters and Mixins/src/App.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
+        <button @click="fruits.push('Strawberry')">Add new fruit</button>
         <p>{{text | toUppercase | to-lowercase}}</p>
         <input v-model="filterText" type="text" />
         <ul>
@@ -23,6 +24,7 @@ export default {
   data() {
     return {
       text: "Hello ! YhjkhJKYuiyhjkhjkh "
+      //fruits: ["lemon"]
     };
   },
   filters: {
