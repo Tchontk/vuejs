13138334267111f0237cb6fcbcbcc8cd28d0ad10
tchontk <tchontk@gmail.com>
2017-11-27 22:19:43 +0100

    #16

---
+++ 16 - Routing in a VueJS Application/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul class="nav nav-tabs">
+      <router-link to="/" tag="li" active-class="active" exact>
+        <a>Home</a>
+      </router-link>
+      <router-link to="/user/1" tag="li" active-class="active">
+        <a>User 1</a>
+      </router-link>
+      <router-link to="/user/2" tag="li" active-class="active">
+        <a>User 2</a>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
