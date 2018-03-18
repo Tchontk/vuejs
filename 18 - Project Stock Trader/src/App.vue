905edfef09fcc714976619784533b805a514300c
Pierrick <tchontk@gmail.com>
2018-03-18 23:51:29 +0100

    Add transition

--- 18 - Project Stock Trader/src/App.vue
+++ 18 - Project Stock Trader/src/App.vue
@@ -30,4 +30,30 @@ export default {
 body {
   padding: 30px;
 }
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-live-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opcacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opcacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    opcacity: 0;
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(-30px);
+    opcacity: 1;
+  }
+}
 </style>
