d3e47243839bf8cad30657f25b9d541296e04c67
Pierrick <tchontk@gmail.com>
2018-03-20 23:35:02 +0100

    GET Dashboard

--- 19 - Using Axios instead of vue-resource/src/components/dashboard/dashboard.vue
+++ 19 - Using Axios instead of vue-resource/src/components/dashboard/dashboard.vue
@@ -2,15 +2,44 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your Email Address : {{ email }}</p>
   </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: ""
+    };
+  },
+  created() {
+    axios
+      .get("https://vuejs-http-10974.firebaseio.com/users.json")
+      .then(response => {
+        const data = response.data;
+        const users = [];
+        for (let key in data) {
+          const user = data[key];
+          user.id = key;
+          users.push(user);
+        }
+        console.log(users);
+        this.email = users[1].email;
+      })
+      .catch(error => console.log(error));
+  }
+};
+</script>
+
 
 <style scoped>
-  h1, p {
-    text-align: center;
-  }
+h1,
+p {
+  text-align: center;
+}
 
-  p {
-    color: red;
-  }
+p {
+  color: red;
+}
 </style>
