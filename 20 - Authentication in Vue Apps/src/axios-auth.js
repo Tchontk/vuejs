23cc01c8c6132d38168a7a7ee831979647f2098b
Pierrick <tchontk@gmail.com>
2018-03-21 23:55:09 +0100

    remove #20

--- 20 - Authentication in Vue Apps/src/axios-auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseUrl: "https://vuejs-http-10974.firebaseio.com"
-})
-
-instance.defaults.headers.common['SOMETHING'] = 'something'
-
-export default instance
