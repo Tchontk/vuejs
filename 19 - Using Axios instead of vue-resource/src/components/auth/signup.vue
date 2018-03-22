48c5913e63cd0f419c63920945eb5759b9ed732a
Pierrick <tchontk@gmail.com>
2018-03-22 23:18:13 +0100

    Modification de l'affichage de l'erreur (blur)

--- 19 - Using Axios instead of vue-resource/src/components/auth/signup.vue
+++ 19 - Using Axios instead of vue-resource/src/components/auth/signup.vue
@@ -4,7 +4,7 @@
       <form @submit.prevent="onSubmit">
         <div class="input" :class="{invalid: $v.email.$error} ">
           <label for="email ">Mail</label>
-          <input type="email " id="email " @input="$v.email.$touch() " v-model="email ">
+          <input type="email " id="email " @blur="$v.email.$touch() " v-model="email ">
           <p v-if="!$v.email.email">Please provide a valid email address.</p>
           <p v-if="!$v.email.required">This field must not be empty.</p>
           <div>{{ $v }}</div>
